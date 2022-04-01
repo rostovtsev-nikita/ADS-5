d42c1ae004a8146cbd4d8afa326f89ec98f00dea
rostovtsev-nikita <99068528+rostovtsev-nikita@users.noreply.github.com>
2022-04-01 21:21:36 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,8 +2,31 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<class T, int size>
 class TStack {
+ 
+ private:
+T arr[20];
+int top;
 
+ public:
+TStack() : top(-1) { }
+T get() const {
+return arr[top];
+}
+bool isEmpty() const {
+return top == -1;
+}
+bool isFull() const {
+return top == size - 1;
+}
+void pop() {
+if (top >= 0)
+top--;
+}
+void push(T item) {
+if (top < size - 1)
+arr[++top] = item;
+}
 };
 #endif  // INCLUDE_TSTACK_H_
